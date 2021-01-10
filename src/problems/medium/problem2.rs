e1c53b3a5c157c0725ee03c5ea70c9ae38bfa224
lflower1206 <lflower1206@gmail.com>
2021-01-10 22:47:15 +0800

    feat: do basic sum with carry

--- src/problems/medium/problem2.rs
+++ src/problems/medium/problem2.rs
@@ -17,7 +17,18 @@ pub fn add_two_numbers(
 ) -> Option<Box<ListNode>> {
     let val1 = l1.unwrap().val;
     let val2 = l2.unwrap().val;
-    let node = ListNode::new(val1 + val2);
+    let sum = val1 + val2;
+    let carry = sum / 10;
+    let val = sum % 10;
+
+    let mut node;
+
+    if carry > 0 {
+        node = ListNode::new(carry);
+        node.next = Some(Box::new(ListNode::new(val)));
+    } else {
+        node = ListNode::new(val);
+    }
 
     Some(Box::new(node))
 }
--- src/problems/medium/problem2_test.rs
+++ src/problems/medium/problem2_test.rs
@@ -12,9 +12,24 @@ mod problem2 {
         let result = problem2::add_two_numbers(Some(boxed_l1), Some(boxed_l2));
 
         match result {
+            Some(result) => assert_eq!(result.val, 2),
+            _ => {}
+        }
+    }
+
+    #[test]
+    fn it_should_get_val_of_six_plus_six() {
+        let l1 = problem2::ListNode { val: 6, next: None };
+        let l2 = problem2::ListNode { val: 6, next: None };
+
+        let boxed_l1 = Box::new(l1);
+        let boxed_l2 = Box::new(l2);
+        let result = problem2::add_two_numbers(Some(boxed_l1), Some(boxed_l2));
+
+        match result {
             Some(result) => {
-                println!("result: {}", result.val);
-                assert_eq!(result.val, 2)
+                assert_eq!(result.val, 1);
+                assert_eq!(result.next.unwrap().val, 2);
             }
             _ => {}
         }
