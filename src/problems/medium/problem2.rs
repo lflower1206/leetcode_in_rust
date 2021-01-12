c670cf526c8a3470352ab6f255af5d422bddc476
lflower1206 <lflower1206@gmail.com>
2021-01-12 23:59:19 +0800

    feat: do the sum for 2 digits

--- src/problems/medium/problem2.rs
+++ src/problems/medium/problem2.rs
@@ -15,20 +15,42 @@ pub fn add_two_numbers(
     l1: Option<Box<ListNode>>,
     l2: Option<Box<ListNode>>,
 ) -> Option<Box<ListNode>> {
-    let val1 = l1.unwrap().val;
-    let val2 = l2.unwrap().val;
-    let sum = val1 + val2;
-    let carry = sum / 10;
-    let val = sum % 10;
+    let node1 = l1.unwrap();
+    let node1_val = &node1.val;
 
-    let mut node;
+    let node2 = l2.unwrap();
+    let node2_val = &node2.val;
 
-    if carry > 0 {
-        node = ListNode::new(carry);
-        node.next = Some(Box::new(ListNode::new(val)));
-    } else {
-        node = ListNode::new(val);
+    let (val, carry) = do_sum(node1_val, node2_val, &0);
+
+    let mut next_carry = carry;
+    let mut node1_next = node1.next.unwrap_or(fulfill_empty_node());
+    let mut node2_next = node2.next.unwrap_or(fulfill_empty_node());
+    let mut result_node = ListNode::new(val);
+
+    while node1_next.val > 0 || node2_next.val > 0 || next_carry > 0 {
+        let (val, carry) = do_sum(&node1_next.val, &node2_next.val, &next_carry);
+        next_carry = carry;
+
+        let mut parent_node = ListNode::new(val);
+        parent_node.next = Some(Box::new(result_node));
+        result_node = parent_node;
+
+        node1_next = node1_next.next.unwrap_or(fulfill_empty_node());
+        node2_next = node2_next.next.unwrap_or(fulfill_empty_node());
     }
 
-    Some(Box::new(node))
+    Some(Box::new(result_node))
+}
+
+pub fn do_sum(val1: &i32, val2: &i32, carry: &i32) -> (i32, i32) {
+    let sum = val1 + val2 + carry;
+    let val = sum % 10;
+    let carry = sum / 10;
+
+    (val, carry)
+}
+
+pub fn fulfill_empty_node() -> Box<ListNode> {
+    Box::new(ListNode::new(0))
 }
--- src/problems/medium/problem2_test.rs
+++ src/problems/medium/problem2_test.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 mod problem2 {
     use crate::problems::medium::problem2;
+    use crate::problems::medium::problem2::ListNode;
 
     #[test]
     fn it_should_get_val_of_one_plus_one() {
@@ -30,6 +31,30 @@ mod problem2 {
             Some(result) => {
                 assert_eq!(result.val, 1);
                 assert_eq!(result.next.unwrap().val, 2);
+            }
+            _ => {}
+        }
+    }
+
+    #[test]
+    fn it_should_get_val_of_twelve_plus_twelve() {
+        let l1 = problem2::ListNode {
+            val: 2,
+            next: Some(Box::new(ListNode { val: 1, next: None })),
+        };
+        let l2 = problem2::ListNode {
+            val: 2,
+            next: Some(Box::new(ListNode { val: 1, next: None })),
+        };
+
+        let boxed_l1 = Box::new(l1);
+        let boxed_l2 = Box::new(l2);
+        let result = problem2::add_two_numbers(Some(boxed_l1), Some(boxed_l2));
+
+        match result {
+            Some(result) => {
+                assert_eq!(result.val, 2);
+                assert_eq!(result.next.unwrap().val, 4);
             }
             _ => {}
         }
