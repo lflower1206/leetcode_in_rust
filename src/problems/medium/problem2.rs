51565482237109c6047e999bebaf569623653daa
lflower1206 <lflower1206@gmail.com>
2021-01-15 00:59:12 +0800

    refactor: problem2

--- src/problems/medium/problem2.rs
+++ src/problems/medium/problem2.rs
@@ -15,87 +15,38 @@ pub fn add_two_numbers(
     l1: Option<Box<ListNode>>,
     l2: Option<Box<ListNode>>,
 ) -> Option<Box<ListNode>> {
-    let node1 = l1.unwrap();
-    let node1_val = &node1.val;
-
-    let node2 = l2.unwrap();
-    let node2_val = &node2.val;
-
-    let (val, carry) = do_sum(node1_val, node2_val, &0);
-
-    let mut next_carry = carry;
-    let mut has_node1_next: bool;
-    let mut has_node2_next: bool;
-    let mut node1_next: Box<ListNode>;
-    let mut node2_next: Box<ListNode>;
-    let mut node_head = Some(Box::new(ListNode::new(val)));
+    let mut node1_current = l1;
+    let mut node2_current = l2;
+    let mut next_carry = 0;
+    let mut node_head = Some(Box::new(ListNode::new(0)));
     let mut node_current = node_head.as_mut();
 
-    match node1.next {
-        Some(node_next) => {
-            has_node1_next = true;
-            node1_next = node_next;
-        }
-        None => {
-            has_node1_next = false;
-            node1_next = fulfill_empty_node()
-        }
-    }
+    while node1_current.is_some() || node2_current.is_some() || next_carry > 0 {
+        let mut sum = next_carry;
 
-    match node2.next {
-        Some(node_next) => {
-            has_node2_next = true;
-            node2_next = node_next;
+        if let Some(node1) = node1_current {
+            sum = sum + node1.val;
+            node1_current = node1.next;
         }
-        None => {
-            has_node2_next = false;
-            node2_next = fulfill_empty_node()
-        }
-    }
 
-    while has_node1_next || has_node2_next || next_carry > 0 {
-        let (val, carry) = do_sum(&node1_next.val, &node2_next.val, &next_carry);
+        if let Some(node2) = node2_current {
+            sum = sum + node2.val;
+            node2_current = node2.next;
+        }
+
+        let (val, carry) = get_val_and_carry(&sum);
+
         next_carry = carry;
 
         if let Some(current) = node_current {
             current.next = Some(Box::new(ListNode::new(val)));
             node_current = current.next.as_mut();
         }
-
-        match node1_next.next {
-            Some(node_next) => {
-                has_node1_next = true;
-                node1_next = node_next;
-            }
-            None => {
-                has_node1_next = false;
-                node1_next = fulfill_empty_node()
-            }
-        }
-
-        match node2_next.next {
-            Some(node_next) => {
-                has_node2_next = true;
-                node2_next = node_next;
-            }
-            None => {
-                has_node2_next = false;
-                node2_next = fulfill_empty_node()
-            }
-        }
     }
 
-    Some(node_head.unwrap())
+    node_head.unwrap().next
 }
 
-pub fn do_sum(val1: &i32, val2: &i32, carry: &i32) -> (i32, i32) {
-    let sum = val1 + val2 + carry;
-    let val = sum % 10;
-    let carry = sum / 10;
-
-    (val, carry)
-}
-
-pub fn fulfill_empty_node() -> Box<ListNode> {
-    Box::new(ListNode::new(0))
+pub fn get_val_and_carry(sum: &i32) -> (i32, i32) {
+    (sum % 10, sum / 10)
 }
